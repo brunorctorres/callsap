9cc31ec599b069e87be98735e92c901f63fa0156
Bruno Torres <brunotorres@bemol.com.br>
2020-08-21 09:58:44 -0400

    Refactoring to add type definitions

--- index.js
+++ index.js
@@ -1,28 +1,29 @@
+"use strict";
+
 const promise = require('request-promise');
 
-module.exports = {
+const rfc = (request) => {
 
-    rfc(request) {
-        
-        return new Promise((resolve, reject) => {
+    return new Promise((resolve, reject) => {
 
-            let headers = {
-                'Content-Type': 'application/json',
-                'token': request.token
-            }
-        
-            let options = {
-                url: request.uri,
-                method: 'POST',
-                headers: headers,
-                body: request.payload,
-                json: true,
-                encoding: 'latin1'
-            }
+        const headers = {
+            'Content-Type': 'application/json',
+            token: request.token
+        }
 
-            promise(options)
-                .then(body => resolve(body))
-                .catch(err => reject(err));
-        });
-    }
+        const options = {
+            url: request.uri,
+            method: 'POST',
+            headers: headers,
+            body: request.payload,
+            json: true,
+            encoding: 'latin1'
+        }
+
+        promise(options)
+            .then(body => resolve(body))
+            .catch(err => reject(err));
+    });
 }
+
+module.exports = rfc;
